14dc4b83a3ad22b987821e201c8f6ea5b474cea5
Jabberdrake <jabberdrake@gmail.com>
2022-12-17 20:17:44 +0000

    Added symlink support to state.c and state.h

--- fs/state.c
+++ fs/state.c
@@ -237,6 +237,11 @@ int inode_create(inode_type i_type) {
         inode_table[inumber].i_size = 0;
         inode_table[inumber].i_data_block = -1;
         break;
+    case T_SYMLINK:
+        // In case of a new symlink, simply sets its size to 0
+        inode_table[inumber].i_size = 0;
+        inode_table[inumber].i_data_block = -1;
+        break;
     default:
         PANIC("inode_create: unknown file type");
     }
