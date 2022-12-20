ca6515b6d36d69765ba6ec30c60123bfe2ceb749
nobnap <m.j.d.f.r.2003@gmail.com>
2022-12-20 11:37:02 +0000

    Added new threading tests

--- new_tests/create_thread_8.c
+++ new_tests/create_thread_8.c
@@ -9,10 +9,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define THREADS 10
+#define THREADS 9
 
 char const file_path[] = "/f";
-char const file_contents[] = "this is a sentence";
+char const local_path[] = "new_tests/test";
+char const external_contents[] = "this is a sentence";
 
 void assert_contents_ok(char const *path, char const *file) {
     int f = tfs_open(path, 0);
@@ -25,23 +26,27 @@ void assert_contents_ok(char const *path, char const *file) {
     assert(tfs_close(f) != -1);
 }
 
-void *alloc_inode(void *num) {
+void *copy_thread(void *num) {
 	int n = *((int *)num);
 
 	char path[sizeof(file_path)+1];
 	memset(path, 0, sizeof(path));
 	strcpy(path, file_path);
-	char content[sizeof(file_contents)+1];
+	char content[sizeof(external_contents)+1];
 	memset(content, 0, sizeof(content));
-	strcpy(content, file_contents);
-
+	strcpy(content, external_contents);
+	char name[sizeof(local_path)+5];
+	memset(name, 0, sizeof(name));
+	strcpy(name, local_path);
+	
 	sprintf(path+2, "%i", n);
 	sprintf(content+18, "%i", n);
+	sprintf(name+14, "%i.txt", n);
 
 	int f = tfs_open(path, TFS_O_CREAT);
     assert(f != -1);
 
-    assert(tfs_write(f, content, sizeof(content)) == sizeof(content));
+    assert(tfs_copy_from_external_fs(name, path) == 0);
 
     assert(tfs_close(f) != -1);
 
@@ -58,7 +63,7 @@ int main() {
 
 	for (int i=0; i < THREADS; i++) {
 		nums[i] = i;
-		if (pthread_create(&tid[i], NULL, alloc_inode, (void *)&nums[i]) != 0) {
+		if (pthread_create(&tid[i], NULL, copy_thread, (void *)&nums[i]) != 0) {
        		fprintf(stderr, "failed to create thread: %s\n", strerror(errno));
        		exit(EXIT_FAILURE);
     	}
